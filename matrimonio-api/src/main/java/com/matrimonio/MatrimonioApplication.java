28e9c363cb8cfaf4a38575f05ef078a97a66cbb1
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2018-11-06 13:04:07 +0100

    Initial Commit

---
+++ matrimonio-api/src/main/java/com/matrimonio/MatrimonioApplication.java
@@ -0,0 +1,12 @@
+package com.matrimonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrimonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrimonioApplication.class, args);
+	}
+}
