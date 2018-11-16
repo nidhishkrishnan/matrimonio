17acf21ec750b80995bc534a7e9a0ab803efa34c
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2018-11-16 10:19:41 +0100

    Adding Swagger and custom validations for Range

---
+++ matrimonio-api/src/main/java/com/matrimonio/validation/InRange.java
@@ -0,0 +1,26 @@
+package com.matrimonio.validation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = { InRangeValidator.class })
+public @interface InRange {
+    String message() default "Must be of a valid range";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    int min() default Integer.MIN_VALUE;
+
+    int max() default Integer.MAX_VALUE;
+}
