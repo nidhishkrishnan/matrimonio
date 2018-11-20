4e0f5e78362bd7f86dd8b62908a9c437d48f565e
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2018-11-20 17:34:07 +0530

    Initial Commit

---
+++ matrimonio-api/src/test/java/com/matrimonio/MatrimonioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.matrimonio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MatrimonioApplicationTests.class)
+public class MatrimonioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
