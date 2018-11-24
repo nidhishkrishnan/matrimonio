a7bee4eafa770c19a412e830ce6e5c35598ed44f
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2018-11-24 13:41:32 +0530

    Adding Constants, User Controller and Controller Advice for profile API's

---
+++ matrimonio-api/src/main/java/com/matrimonio/common/MatrimonioConstants.java
@@ -0,0 +1,15 @@
+package com.matrimonio.common;
+
+public final class MatrimonioConstants {
+
+	private MatrimonioConstants() {
+	}
+
+	public static final String AGE					= "age";
+	public static final String HEIGHT_IN_CM			= "heightInCm";
+	public static final String DISPLAY_NAME 		= "displayName";
+	public static final String COMPATIBILITY_SCORE 	= "compatibilityScore";
+	public static final String FAVOURITE		 	= "favourite";
+	public static final String MAIN_PHOTO		 	= "mainPhoto";
+	public static final String CONTACTS_EXCHANGED 	= "contactsExchanged";
+}
---
+++ matrimonio-api/src/main/java/com/matrimonio/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.matrimonio.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.matrimonio.domain.FilterProfileRequest;
+import com.matrimonio.domain.UserProfileDisplayNames;
+import com.matrimonio.model.Profile;
+import com.matrimonio.service.ProfileService;
+
+@RestController
+@RequestMapping("v1/profile")
+public class UserController {
+
+	private ProfileService profileService;
+	
+	public UserController(ProfileService profileService) {
+		this.profileService = profileService;
+	}
+	
+	@GetMapping("/list")
+	public List<Profile> getAllProfiles(@Valid @RequestParam final String loginUserName) {
+		return this.profileService.getAllProfiles(loginUserName);
+	}
+	
+	@GetMapping("/names")
+	public List<UserProfileDisplayNames> getAllUserDisplayNames() {
+		return this.profileService.getAllUserDisplayNames();
+	}
+	
+	@PostMapping("/filter")
+	public List<Profile> filterProfiles(@Valid @RequestParam String loginUserName, @Valid @RequestBody final FilterProfileRequest filterProfileRequest) {
+		return this.profileService.filterProfiles(loginUserName, filterProfileRequest);
+	}
+}
