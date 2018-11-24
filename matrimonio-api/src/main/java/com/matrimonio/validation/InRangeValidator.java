6a8b0edf663f4cb3172b5b1b516b0933a6ed3992
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2018-11-24 14:49:41 +0530

    Adding Swagger and custom validations for Range

---
+++ matrimonio-api/src/main/java/com/matrimonio/validation/InRangeValidator.java
@@ -0,0 +1,26 @@
+package com.matrimonio.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.stereotype.Component;
+
+import com.matrimonio.domain.Range;
+
+@Component
+public class InRangeValidator implements ConstraintValidator<InRange, Range> {
+
+	private int min;
+	private int max;
+
+	@Override
+	public void initialize(InRange constraintAnnotation) {
+		this.min = constraintAnnotation.min();
+		this.max = constraintAnnotation.max();
+	}
+
+	@Override
+	public boolean isValid(Range value, ConstraintValidatorContext context) {
+		return value == null || (value.getStart() >= this.min && value.getEnd() < this.max && value.getEnd() > this.min);
+	}
+}
