af166b9b62bc20d2154b6276f930926442671545
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2018-11-25 19:24:45 +0530

    Refactoring environments Adding assets related files and styles

--- matrimonio-spa/src/environments/environment.ts
+++ matrimonio-spa/src/environments/environment.ts
@@ -1,19 +1,14 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
   production: false,
   endPoints: {
-    getAllUserDisplayNames:{
-      url: 'http://localhost:8080/matrimonio/v1/user/names'
+    getAllUserDisplayNames: {
+      url: 'http://localhost:8080/matrimonio/v1/profile/names'
     },
-    getAllProfiles:{
+    getAllProfiles: {
       url: 'http://localhost:8080/matrimonio/v1/profile/list'
     },
-    category:{
-      url: 'http://localhost:8080/matrimonio/category'
+    filterProfiles: {
+      url: 'http://localhost:8080/matrimonio/v1/profile/filter'
     }
   }
 };
