116b9312ba3a17a4d5de9ee11752c39c96b2e3f4
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2018-11-27 19:00:50 +0100

    Refactoring H2 data from string null to null

--- matrimonio-api/src/main/resources/data.sql
+++ matrimonio-api/src/main/resources/data.sql
@@ -38,13 +38,13 @@ INSERT  INTO `profile`(`display_name`,`age`,`name`,`coordinate`,`compatibility_s
 ('Katherine',39,'London','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a400602000244000178440001797870bfbe3b46fdeb52ca4049c143419e3001',0.99,50,'TRUE',177,'Lawyer','http://thecatapi.com/api/images/get?format=src&type=gif','Atheist');
 
 INSERT  INTO `profile`(`display_name`,`age`,`name`,`coordinate`,`compatibility_score`,`contacts_exchanged`,`favourite`,`height_in_cm`,`job_title`,`main_photo`,`religion`) VALUES 
-('Katie',40,'Weymouth','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a400602000244000178440001797870c003a93532e7b3d940494ea59c065b64',0.94,0,'FALSE',148,'Lawyer','null','Atheist');
+('Katie',40,'Weymouth','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a400602000244000178440001797870c003a93532e7b3d940494ea59c065b64',0.94,0,'FALSE',148,'Lawyer',null,'Atheist');
 
 INSERT  INTO `profile`(`display_name`,`age`,`name`,`coordinate`,`compatibility_score`,`contacts_exchanged`,`favourite`,`height_in_cm`,`job_title`,`main_photo`,`religion`) VALUES 
 ('Katlin',39,'Inverness','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a400602000244000178440001797870c010e61d4306e5cd404cbd27a20578e6',0.87,0,'TRUE',153,'Barrister','http://thecatapi.com/api/images/get?format=src&type=gif','Jewish');
 
 INSERT  INTO `profile`(`display_name`,`age`,`name`,`coordinate`,`compatibility_score`,`contacts_exchanged`,`favourite`,`height_in_cm`,`job_title`,`main_photo`,`religion`) VALUES 
-('Kysha',45,'London','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a400602000244000178440001797870bfbe3b46fdeb52ca4049c143419e3001',0.88,10,'TRUE',144,'Lawyer','null','Islam');
+('Kysha',45,'London','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a400602000244000178440001797870bfbe3b46fdeb52ca4049c143419e3001',0.88,10,'TRUE',144,'Lawyer',null,'Islam');
 
 INSERT  INTO `profile`(`display_name`,`age`,`name`,`coordinate`,`compatibility_score`,`contacts_exchanged`,`favourite`,`height_in_cm`,`job_title`,`main_photo`,`religion`) VALUES 
 ('Laura',39,'Plymouth','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a400602000244000178440001797870c010934b0e1b4bb64049302a3cea6c1a',0.89,0,'FALSE',160,'Lawyer','http://thecatapi.com/api/images/get?format=src&type=gif','Christian');
@@ -65,7 +65,7 @@ INSERT  INTO `profile`(`display_name`,`age`,`name`,`coordinate`,`compatibility_s
 ('Sharon',47,'Solihull','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a400602000244000178440001797870bffc737eb28d8666404a34d6fda836eb',0.97,0,'FALSE',161,'Doctor','http://thecatapi.com/api/images/get?format=src&type=gif','Islam');
 
 INSERT  INTO `profile`(`display_name`,`age`,`name`,`coordinate`,`compatibility_score`,`contacts_exchanged`,`favourite`,`height_in_cm`,`job_title`,`main_photo`,`religion`) VALUES 
-('Stephanie',39,'London','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a400602000244000178440001797870bfbe3b46fdeb52ca4049c143419e3001',0.87,4,'FALSE',153,'Project Manager','null','Christian');
+('Stephanie',39,'London','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a400602000244000178440001797870bfbe3b46fdeb52ca4049c143419e3001',0.87,4,'FALSE',153,'Project Manager',null,'Christian');
 
 INSERT  INTO `profile`(`display_name`,`age`,`name`,`coordinate`,`compatibility_score`,`contacts_exchanged`,`favourite`,`height_in_cm`,`job_title`,`main_photo`,`religion`) VALUES 
 ('Susan',25,'Harlow','aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a4006020002440001784400017978703fba30fcf80dc3374049e2efa1e3eaf7',0.88,0,'FALSE',166,'Project Manager','http://thecatapi.com/api/images/get?format=src&type=gif','Christian');
